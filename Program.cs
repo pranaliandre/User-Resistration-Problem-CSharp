165b32cc19b905c7d14bdaf1d7a67f3a381f75b0
pranaliandre <pranaliandre06@gmail.com>
2020-03-31 15:01:17 -0500

    Added: Welcome to user registration

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace User_Registration_Problem_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration");
+        }
+    }
+}
