7b3b361850dbd4beb09026148135c27c3c52b965
pranaliandre <pranaliandre06@gmail.com>
2020-04-01 13:11:23 -0500

    Added: Validation of Email Address

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace User_Registration_Problem_CSharp
         //method to take first name from user
         public string inputFirstName()
         {
-            Console.Write("Enter Name: ");//taking first name from user 
+            Console.Write("Enter First Name: ");//taking first name from user 
             String firstName = Console.ReadLine();
             return firstName;
         }
@@ -34,9 +34,22 @@ namespace User_Registration_Problem_CSharp
         //method to take last name from user
         public string inputLastName()
         {
-            Console.Write("Enter Name: ");//taking last name from user 
+            Console.Write("Enter Last Name: ");//taking last name from user 
             String lastName = Console.ReadLine();
             return lastName;
+        }
+        //method to take email address from user
+        public string inputEmailAddress()
+        {
+            Console.Write("Enter Email Address: ");//taking email address from user 
+            String emailAddress = Console.ReadLine();
+            return emailAddress;
+        }
+        //method to validate Email Address
+        public string validationEmailAddress(string emailAddress)
+        {
+            var regexExpr = @"^([a-zA-Z]{3,}([.|_|+|-]?[a-zA-Z0-9]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.]?[a-zA-Z]{2,3})?)$";
+            return this.validationCheck(regexExpr, emailAddress);
         }
         static void Main(string[] args)
         {
@@ -46,6 +59,8 @@ namespace User_Registration_Problem_CSharp
             Console.WriteLine($"First name {user.validationName(firstName)}");
             string lastName = user.inputLastName();
             Console.WriteLine($"Last name {user.validationName(lastName)}");
+            string emailAddress = user.inputEmailAddress();
+            Console.WriteLine($"Email address {user.validationEmailAddress(emailAddress)}");
         }
     }
 }
