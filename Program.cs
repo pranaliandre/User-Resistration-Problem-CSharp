ae69aade48fb922aec5686d1227aa0f87b232e4c
pranaliandre <pranaliandre06@gmail.com>
2020-04-01 12:31:29 -0500

    Added: Validation of last name

--- Program.cs
+++ Program.cs
@@ -5,12 +5,17 @@ namespace User_Registration_Problem_CSharp
 {
     class Program
     {
-        static void Main(string[] args)
+        public string validationCheck(string exp, string stringToMatch)
         {
-            Program user = new Program();
-            Console.WriteLine("Welcome to User Registration");
-            string firstName=user.inputFirstName();
-            user.validationFirstName(firstName);
+            var match = Regex.Match(stringToMatch, exp, RegexOptions.None);
+            if (match.Success)
+            {
+                return "Valid";
+            }
+            else
+            {
+                return "Invalid";
+            }
         }
         //method to take first name from user
         public string inputFirstName()
@@ -19,19 +24,28 @@ namespace User_Registration_Problem_CSharp
             String firstName = Console.ReadLine();
             return firstName;
         }
-        //method to validation of first name
-        public void validationFirstName(string firstName)
+        //method to validation of name
+        public string validationName(string name)
         {
             var regexExpr = @"^[A-Z]{1}[a-z]{2,}$";
-            var match = Regex.Match(firstName, regexExpr,RegexOptions.None);
-            if(match.Success)
-            {
-                Console.WriteLine("Valid firstname");
-            }
-            else
-            {
-                Console.WriteLine("Invalid firstname");
-            }
+            return this.validationCheck(regexExpr,name);
+        }
+
+        //method to take last name from user
+        public string inputLastName()
+        {
+            Console.Write("Enter Name: ");//taking last name from user 
+            String lastName = Console.ReadLine();
+            return lastName;
+        }
+        static void Main(string[] args)
+        {
+            Program user = new Program();
+            Console.WriteLine("Welcome to User Registration");
+            string firstName = user.inputFirstName();
+            Console.WriteLine($"First name {user.validationName(firstName)}");
+            string lastName = user.inputLastName();
+            Console.WriteLine($"Last name {user.validationName(lastName)}");
         }
     }
 }
